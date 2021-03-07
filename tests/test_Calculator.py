0d24d5e07df3cee97427341cd24c4dd2dc78f044
mohdakibuddin <mohdakibuddin@gmail.com>
2021-03-07 14:16:35 -0500

    Fix: Refactored test_Calculator.py so that it is compatible with refactored calculator.py

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -1,4 +1,3 @@
-
 # Move Imports to fileReader.py ^^
 import unittest
 
@@ -7,11 +6,12 @@ import unittest
 # Move Section to fileReader.py
 from src.calculator import Calculator
 
-from tests.src.fileReader import FileReader
+from tests.src.CSVReader import readCSV
 
 '''
 This is the TestCase class that test Calculator class functions.
 '''
+
 
 class TestCalculator(unittest.TestCase):
     # this is the Calculator class instance.
@@ -32,7 +32,6 @@ class TestCalculator(unittest.TestCase):
     # execute before every test case function run.
     def setUp(self):
         self.calculator = Calculator()
-        self.fileReader = FileReader()
         print('')
         print('setUp')
 
@@ -41,94 +40,66 @@ class TestCalculator(unittest.TestCase):
         # release the Calculator object.
         if self.calculator is not None:
             self.calculator = None
-        self.fileReader.closeFile()
         print('')
         print('tearDown')
 
     # below are function that test Calculator class's plus, minus, multiple and divide functions.
-    def test_addition(self):
-        print('')
-        print('******test_addition******')
-        # get each row text from the csv file.
-        for row in self.fileReader.openFile("testCases/Unit_Test_Addition.csv"):
-            # the first column in the text line is x value.
-            x = row[0]
-            # the second column in the text line is y value.
-            y = row[1]
-            # the third column in the text line is (x + y) value.
-            expect_result = row[2]
-            result = self.calculator.sum(x, y)
+    def testInstantiateCalculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
 
-            print(str(x) + ' + ' + str(y) + ' = ' + str(result) + ', expect ' + str(expect_result))
-            self.assertEqual(float(result), float(expect_result))
+    def testAddition(self):
+        rows = readCSV("tests/testCases/Addition.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            x = float(row["Value 1"])
+            y = float(row["Value 2"])
+            self.assertEqual(self.calculator.add(x, y), expectedResult)
 
-    def test_subtraction(self):
-        print('')
-        print('******test_subtraction******')
-        for row in self.fileReader.openFile("testCases/Unit_Test_Subtraction.csv"):
-            x = row[0]
-            y = row[1]
-            expect_result = row[2]
-            result = self.calculator.subtract(x, y)
+    def testSubtraction(self):
+        rows = readCSV("tests/testCases/Subtraction.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            y = float(row["Value 1"])
+            x = float(row["Value 2"])
+            self.assertEqual(self.calculator.subtract(x, y), expectedResult)
 
-            print(str(x) + ' - ' + str(y) + ' = ' + str(result) + ', expect ' + str(expect_result))
-            self.assertAlmostEqual(float(result), float(expect_result))
+    def testDivision(self):
+        self.assertRaises(ZeroDivisionError, self.calculator.divide, 1, 0)
+        rows = readCSV("tests/testCases/Division.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            y = float(row["Value 1"])
+            x = float(row["Value 2"])
+            self.assertAlmostEqual(self.calculator.divide(x, y), expectedResult)
 
-    def test_multiplication(self):
-        print('')
-        print('******test_multiplication******')
-        for row in self.fileReader.openFile("testCases/Unit_Test_Multiplication.csv"):
-            x = row[0]
-            y = row[1]
-            # the fifth column in the text line is (x * y) value.
-            expect_result = row[2]
-            result = self.calculator.multiply(x, y)
+    def testMultiplication(self):
+        rows = readCSV("tests/testCases/Multiplication.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            x = float(row["Value 1"])
+            y = float(row["Value 2"])
+            self.assertAlmostEqual(self.calculator.multiply(x, y), expectedResult)
 
-            print(str(x) + ' * ' + str(y) + ' = ' + str(result) + ', expect ' + str(expect_result))
-            self.assertEqual(float(result), float(expect_result))
+    def testSquare(self):
+        rows = readCSV("tests/testCases/Square.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            x = float(row["Value 1"])
+            self.assertAlmostEqual(self.calculator.square(x), expectedResult)
 
-    def test_division(self):
-        print('')
-        print('******test_division******')
-        for row in self.fileReader.openFile("testCases/Unit_Test_Division.csv"):
-            x = row[0]
-            y = row[1]
-            # the sixth column in the text line is (x / y) value.
-            expect_result = row[2]
-            result = self.calculator.divide(x, y)
+    def testSquareRoot(self):
+        rows = readCSV("tests/testCases/SquareRoot.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            x = float(row["Value 1"])
+            self.assertAlmostEqual(self.calculator.squareRoot(x), expectedResult)
 
-            print(str(x) + ' / ' + str(y) + ' = ' + str(result) + ', expect ' + str(expect_result))
-            if isinstance(result, float):
-                self.assertAlmostEqual(result, float(expect_result))
-            else:
-                self.assertEqual(result, expect_result)
+    def testResultProperty(self):
+        self.calculator.results.clear()
+        self.assertEqual(self.calculator.results, [])
 
 
-    def test_squareRoot(self):
-        print('')
-        print('******test_squareRoot******')
-        for row in self.fileReader.openFile("testCases/Unit_Test_Square_Root.csv"):
-            x = row[0]
-            expect_result = row[1]
-            result = self.calculator.root(x, 2)
-
-            print(' √ ' + str(x) + ' = ' + str(result) + ', expect ' + str(expect_result))
-            self.assertEqual(float(result), float(expect_result))
-
-    def test_squared(self):
-        print('')
-        print('******test_squared******')
-        for row in self.fileReader.openFile("testCases/Unit_Test_Square.csv"):
-            x = row[0]
-            # the sixth column in the text line is (x / y) value.
-            expect_result = row[1]
-            result = self.calculator.power(x, 2)
-
-            print(str(x) + '² ' + ' = ' + str(result) + ', expect ' + str(expect_result))
-            self.assertEqual(float(result), float(expect_result))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
 
 
