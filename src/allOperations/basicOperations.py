17b0e583e92d3914c699d90a5519a5c4a3a06053
mohdakibuddin <mohdakibuddin@gmail.com>
2021-03-07 13:00:23 -0500

    Fix: refactored basic operations to be static methods in basicOperations.py

---
+++ src/allOperations/basicOperations.py
@@ -0,0 +1,33 @@
+class BasicOperations:
+    @staticmethod
+    def addition(x, y):
+        result = float(x) + float(y)
+        return result
+
+    @staticmethod
+    def subtraction(x, y):
+        result = float(y) - float(x)
+        return result
+
+    @staticmethod
+    def multiplication(x, y):
+        result = y * x
+        return result
+
+    @staticmethod
+    def division(x, y):
+        result = float(y) / float(x)
+        return result
+
+    @staticmethod
+    def square(x):
+        result = x ** 2
+        return result
+
+    @staticmethod
+    def squareRoot(x):
+        if x < 0:
+            print("Cannot output imaginary result, please enter a positive number for x")
+        else:
+            result = x ** .5
+            return result
