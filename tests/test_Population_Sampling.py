080c6f48d991b2d03d1f2b10f065bf5c987a8166
javier <johnnybelesaca@gmail.com>
2021-03-07 23:45:42 -0500

    Fix: reformatted file

--- tests/test_Population_Sampling.py
+++ tests/test_Population_Sampling.py
@@ -6,19 +6,21 @@ from src.population_sampling_calculator import Population_Sampling_Calculator
 
 seedNum = 50
 
+
 def random_list():
     random.seed(seedNum)
     original_list = []
-    #original_list = randint(0, 50, 15)
-    #return original_list
+    # original_list = randint(0, 50, 15)
+    # return original_list
     # DONT CHNAGE THE 50 cause then everything has to change
     for x in range(50):
         original_list.append(float(random.randint(40, 50)))
     return original_list
 
+
 class SamplingTestCase(unittest.TestCase):
-    #original_list = []
-    #seedNum = 0
+    # original_list = []
+    # seedNum = 0
     '''
     def setUp(self) -> None:
         random.seed(5)
@@ -34,9 +36,8 @@ class SamplingTestCase(unittest.TestCase):
         original_list = random_list()
         sample_list = Population_Sampling_Calculator.random_sampling(original_list, 8)
         print("Random Sample List")
-        print(sample_list,"\n")
+        print(sample_list, "\n")
         self.assertEqual(sample_list, [41.0, 50.0, 47.0, 43.0, 42.0, 49.0, 47.0, 43.0])
-
 
     def test_confidence_interval_for_sample(self):
         print("******test_random_sampling******")
@@ -44,7 +45,7 @@ class SamplingTestCase(unittest.TestCase):
         sample_list = Population_Sampling_Calculator.random_sampling(original_list, len(original_list))
         result = Population_Sampling_Calculator.confidence_interval_for_sample(0.9, sample_list)
         print("Confidence Interval for Sample List")
-        print(result,"\n")
+        print(result, "\n")
         self.assertAlmostEqual(result, (44.43924576646906, 45.840754233530944))
 
     def test_margin_error(self):
