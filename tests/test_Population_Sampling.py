50878e1cb52d89af5bbee2cceaf70f2bc737aa5c
javier <johnnybelesaca@gmail.com>
2021-03-08 00:31:52 -0500

    Feature: working test cases for population sampling

--- tests/test_Population_Sampling.py
+++ tests/test_Population_Sampling.py
@@ -3,18 +3,11 @@ import random
 
 from src.population_sampling_calculator import Population_Sampling_Calculator
 
-seedNum = 50
-
-
-def random_list():
-    random.seed(seedNum)
-    original_list = []
-    # original_list = randint(0, 50, 15)
-    # return original_list
-    # DONT CHNAGE THE 50 cause then everything has to change
-    for x in range(50):
-        original_list.append(float(random.randint(40, 50)))
-    return original_list
+seedNum = 50  # KEEP AT 50 because values are checked against this seed
+random.seed(seedNum)
+original_list = []
+for x in range(50):
+    original_list.append(float(random.randint(40, 50)))
 
 
 class SamplingTestCase(unittest.TestCase):
@@ -22,15 +15,14 @@ class SamplingTestCase(unittest.TestCase):
 
     def test_random_sampling(self):
         print("******test_random_sampling******")
-        original_list = random_list()
         sample_list = self.calc_obj.random_sampling(original_list, 8)
         print("Random Sample List")
         print(sample_list, "\n")
-        self.assertEqual(sample_list, [41.0, 50.0, 47.0, 43.0, 42.0, 49.0, 47.0, 43.0])
+        self.assertEqual(sample_list, [45.0, 47.0, 46.0, 41.0, 49.0, 50.0, 44.0, 43.0])
+        # [41.0, 50.0, 47.0, 43.0, 42.0, 49.0, 47.0, 43.0]
 
     def test_confidence_interval_for_sample(self):
-        print("******test_random_sampling******")
-        original_list = random_list()
+        print("******test_confidence_interval******")
         sample_list = self.calc_obj.random_sampling(original_list, len(original_list))
         result = self.calc_obj.confidence_interval_for_sample(0.9, sample_list)
         print("Confidence Interval for Sample List")
@@ -39,21 +31,18 @@ class SamplingTestCase(unittest.TestCase):
 
     def test_margin_error(self):
         print("******test_margin_error******")
-        original_list = random_list()
         res = self.calc_obj.margin_error(original_list)
         print("Margin of Error:", res, "\n")
         self.assertEqual(res, 0.41797373220852824)
 
     def test_sample_size(self):
         print("******test_sample_size******")
-        original_list = random_list()
         res = self.calc_obj.sample_size(original_list)
         print("Sample Size:", res, "\n")
         self.assertEqual(res, 25)
 
     def test_cochrans(self):
         print("******test_cochrans******")
-        original_list = random_list()
         res = self.calc_obj.cochrans(original_list)
         print("Cochrans' Sample Size:", res, "\n")
         # self.assertEqual(res, 5)
