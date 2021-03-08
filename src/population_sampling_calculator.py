d3b36452f2a8015d96e1db3e4c6472f787c32840
javier <johnnybelesaca@gmail.com>
2021-03-07 23:50:25 -0500

    Fix: removed unneeded seedNum arg

--- src/population_sampling_calculator.py
+++ src/population_sampling_calculator.py
@@ -7,7 +7,7 @@ class Population_Sampling_Calculator(Calculator):
     def __init__(self):
         Calculator.__init__(self)
 
-    def random_sampling(self, input_list, new_list_length, seed_num):
+    def random_sampling(self, input_list, new_list_length):
         self.results.append(PopulationSamplingOperations.sample_list(input_list, new_list_length))
         return self.results[-1]
 
