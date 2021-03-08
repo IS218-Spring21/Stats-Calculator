0d7fdf85666ea738cd4ce4f66ca35106221a042c
javier <johnnybelesaca@gmail.com>
2021-03-08 00:11:23 -0500

    Fix: created calculator object: calc_obj and used it for all test cases

--- tests/test_Population_Sampling.py
+++ tests/test_Population_Sampling.py
@@ -19,6 +19,7 @@ def random_list():
 
 
 class SamplingTestCase(unittest.TestCase):
+    calc_obj = Population_Sampling_Calculator()
     # original_list = []
     # seedNum = 0
     """
@@ -34,7 +35,7 @@ class SamplingTestCase(unittest.TestCase):
     def test_random_sampling(self):
         print("******test_random_sampling******")
         original_list = random_list()
-        sample_list = Population_Sampling_Calculator.random_sampling(original_list, 8)
+        sample_list = self.calc_obj.random_sampling(original_list, 8)
         print("Random Sample List")
         print(sample_list, "\n")
         self.assertEqual(sample_list, [41.0, 50.0, 47.0, 43.0, 42.0, 49.0, 47.0, 43.0])
@@ -42,8 +43,8 @@ class SamplingTestCase(unittest.TestCase):
     def test_confidence_interval_for_sample(self):
         print("******test_random_sampling******")
         original_list = random_list()
-        sample_list = Population_Sampling_Calculator.random_sampling(original_list, len(original_list))
-        result = Population_Sampling_Calculator.confidence_interval_for_sample(0.9, sample_list)
+        sample_list = self.calc_obj.random_sampling(original_list, len(original_list))
+        result = self.calc_obj.confidence_interval_for_sample(0.9, sample_list)
         print("Confidence Interval for Sample List")
         print(result, "\n")
         self.assertAlmostEqual(result, (44.43924576646906, 45.840754233530944))
@@ -51,21 +52,21 @@ class SamplingTestCase(unittest.TestCase):
     def test_margin_error(self):
         print("******test_margin_error******")
         original_list = random_list()
-        res = Population_Sampling_Calculator.margin_error(original_list)
+        res = self.calc_obj.margin_error(original_list)
         print("Margin of Error:", res, "\n")
         self.assertEqual(res, 0.41797373220852824)
 
     def test_sample_size(self):
         print("******test_sample_size******")
         original_list = random_list()
-        res = Population_Sampling_Calculator.sample_size(original_list)
+        res = self.calc_obj.sample_size(original_list)
         print("Sample Size:", res, "\n")
         self.assertEqual(res, 25)
 
     def test_cochrans(self):
         print("******test_cochrans******")
         original_list = random_list()
-        res = Population_Sampling_Calculator.cochrans(original_list)
+        res = self.calc_obj.cochrans(original_list)
         print("Cochrans' Sample Size:", res, "\n")
         # self.assertEqual(res, 5)
 
